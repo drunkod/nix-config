f28f5cd7a98abef4884225995f710f7aa38e6939
John Doe <johndoe@example.com>
2024-02-02 01:45:53 +0500

    add mpv

--- lenovo/home-manager/home.nix
+++ lenovo/home-manager/home.nix
@@ -54,7 +54,7 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [ mpv ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
