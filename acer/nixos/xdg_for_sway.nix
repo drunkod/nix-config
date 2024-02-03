33ba988bda7ead1bed1650594abf754901ffa497
drunkod <drunkod@gmail.com>
2024-02-03 19:14:25 +0500

    fix https://www.reddit.com/r/NixOS/comments/184hbt6/changes_to_xdgportals/?rdt=44485

--- acer/nixos/xdg_for_sway.nix
+++ acer/nixos/xdg_for_sway.nix
@@ -6,10 +6,12 @@
 xdg = {
     portal = {
       enable = true;
+    config = {  
       extraPortals = with pkgs; [
         xdg-desktop-portal-wlr
         xdg-desktop-portal-gtk
       ];
+      };
     };
   };
 
