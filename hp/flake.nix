ebada7d582c23c31f04e717989add80771e754f4
drunkod <drunkod@gmail.com>
2025-01-18 17:43:08 +0500

    Update flake.nix

--- hp/flake.nix
+++ hp/flake.nix
@@ -23,7 +23,7 @@
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
       # FIXME replace with your hostname
-      acer = nixpkgs.lib.nixosSystem {
+      hp = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         # > Our main nixos configuration file <
         modules = [./nixos/configuration.nix];
@@ -34,7 +34,7 @@
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
       # FIXME replace with your username@hostname
-      "alex@acer" = home-manager.lib.homeManagerConfiguration {
+      "alex@hp" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
